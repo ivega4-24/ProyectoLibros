f50b14bd502dd599a02eef0491693dfeb80c66d3
Isa Vega <ivegadelahoz.8s@gmail.com>
2024-11-12 19:44:14 -0500

    inicioyacercade

--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -1,9 +1,122 @@
+import fotoUno from "../../../../src/assets/img/slide1.jpg";
+import fotoDos from "../../../../src/assets/img/slide2.jpg";
+import fotoTres from "../../../../src/assets/img/slide3.jpg";
+import cardUno from "../../../../src/assets/img/lini1.jpg";
+import cardDos from "../../../../src/assets/img/lini2.jpg";
+import cardTres from "../../../../src/assets/img/lini3.jpg";
+
 export const Inicio = () => {
   return (
     <>
-      
-      Hola yo soy el Inicio <br />
-      Como el de la bienvenida...
+      <div className="pt-4 d-flex justify-content-center">
+        <div className="col-md-10">
+          <div id="carouselExampleIndicators" className="carousel slide">
+            <div className="carousel-indicators">
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="0"
+                className="active"
+                aria-current="true"
+                aria-label="Slide 1"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="1"
+                aria-label="Slide 2"
+              ></button>
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="2"
+                aria-label="Slide 3"
+              ></button>
+            </div>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img
+                  src={fotoUno}
+                  className="d-block w-100"
+                  alt="Esta es la Slide 1"
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src={fotoDos}
+                  className="d-block w-100"
+                  alt="Esta es la Slide 2"
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src={fotoTres}
+                  className="d-block w-100"
+                  alt="Esta es la Slide 3"
+                />
+              </div>
+            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+
+        <h1 className="pt-4 d-flex justify-content-center">Best Sellers</h1>
+
+
+        <div className="pt-4 d-flex justify-content-center">
+        <div className="col-md-8">
+            <div className="card-group">
+  <div className="card ">
+    <img src={cardUno} className="card-img-top" alt="Bestseller1" />
+    <div className="card-body">
+      <h5 className="card-title">Bestseller Uno</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <img src={cardDos} className="card-img-top" alt="Bestseller2" />
+    <div className="card-body">
+      <h5 className="card-title">Bestseller Dos</h5>
+      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <img src={cardTres} className="card-img-top" alt="Bestseller3" />
+    <div className="card-body">
+      <h5 className="card-title">Bestseller Tres</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+     </div>
+     </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/app/componentes/otros/AcercaDe.tsx
+++ src/app/componentes/otros/AcercaDe.tsx
@@ -1,8 +1,64 @@
+import acercadeslide from "../../../assets/img/slidefaq.jpg"
+
 export const AcercaDe = () => {
     return (
-        <>
-         Hola yo soy acerca de <br />
-         El de la información...
-        </>
+      <>
+        <div className="pt-4 d-flex justify-content-center">
+          <div className="col-md-10">
+            <div id="carouselExampleIndicators" className="carousel slide">
+              <div className="carousel-indicators">
+              </div>
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img
+                    src={acercadeslide}
+                    className="d-block w-100"
+                    alt="Esta es la Slide 1"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="accordion" id="accordionExample">
+  <div className="accordion-item">
+    <h2 className="accordion-header">
+      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      ¿Cuánto tiempo tarda en llegar un libro desde el momento de la compra?
+      </button>
+    </h2>
+    <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+      El tiempo de envío depende de tu ubicación y del método de entrega que elijas. En general, los envíos nacionales tardan entre 3 a 5 días hábiles, mientras que los envíos internacionales pueden demorar entre 7 y 14 días. Recibirás un número de rastreo para que puedas seguir tu pedido en todo momento.
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+      ¿Qué hago si el libro que quiero está agotado?
+      </button>
+    </h2>
+    <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+      Si el libro que buscas está agotado, puedes suscribirte a nuestra notificación de disponibilidad en la página del producto. Te avisaremos por correo electrónico en cuanto esté disponible nuevamente. También puedes contactarnos para ver si es posible hacer un pedido especial.
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+      ¿Puedo devolver un libro si no estoy satisfecho con la compra?
+      </button>
+    </h2>
+    <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+      Sí, ofrecemos una política de devoluciones para garantizar tu satisfacción. Si el libro no cumple con tus expectativas o llega en malas condiciones, tienes hasta 30 días para realizar una devolución o solicitar un cambio. Solo necesitas conservar el comprobante de compra y asegurarte de que el libro esté en buen estado.
+      </div>
+    </div>
+  </div>
+</div>
+          </div>
+        </div>
+      </>
     );
-};
+  };
+  
